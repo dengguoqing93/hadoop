f6c387e8561eeadd8f14d25543cdfcbb9095d64d
dengguoqing <dengguoqing93@163.com>
2019-02-18 08:33:17 +0800

    运行命令

--- algorithms/sorted/src/main/java/run.sh
+++ algorithms/sorted/src/main/java/run.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+export ALGORITHMS_HOME=/root/algorithms/
+export APP_JAR=${ALGORITHMS_HOME}/jar/sorted-1.0-SNAPSHOT.jar
+INPUT=hdfs://master:9000/secondary_sort/input
+OUTPUT=hdfs://master:9000/secondary_sort/output
+hadoop fs -rm ${INPUT}/sample_input.txt
+hadoop fs -copyFromLocal ${ALGORITHMS_HOME}/input/sample_input.txt ${INPUT}/sample_input.txt
+hadoop fs -rm -r ${OUTPUT}
+PROG=job.SecondarySortDriver
+hadoop jar ${APP_JAR} ${PROG} ${INPUT} ${OUTPUT}
